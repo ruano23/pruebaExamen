830cedf54015cceeb5b439fca0caa1db1a823b58
Cristian <32903920+crigalbla@users.noreply.github.com>
2018-01-18 12:35:53 +0100

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -9,8 +9,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 public class Application {
 
     public static void main(String[] args) {
-        
-        SpringApplication.run( Application.class, args );
+        SpringApplication.run(Application.class, args);
     }
 
 }
